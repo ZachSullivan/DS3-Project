d21aca8f7206b71e9e9256df5f7eab28cb079ee5
TylerDinardo <dtyler9@My-MacBook-Pro.local>
2015-03-06 08:13:21 -0500

    Bird Animations
    Animations for Birds. Man I’m good at these descriptions.

---
+++ BanjoGod/Assets/BirdChildController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdChildController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
