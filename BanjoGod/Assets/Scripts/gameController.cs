35134257dc8c8bff96a50cd310155878d069b867
ZachSullivan <zach.r.sullivan@gmail.com>
2015-04-14 20:50:50 -0400

    Fixed Glitchy snowFade

--- BanjoGod/Assets/Scripts/gameController.cs
+++ BanjoGod/Assets/Scripts/gameController.cs
@@ -202,8 +202,9 @@ public class gameController : MonoBehaviour {
 		
 		WinterIsland.GetComponent<Animator>().SetBool("isWinter", false);
 		
-		yield return new WaitForSeconds (3.0f);
-		
+		yield return new WaitForSeconds (1.0f);
+
+
 		WinterIsland.GetComponent<SpriteRenderer>().enabled = false;
 	}
 
